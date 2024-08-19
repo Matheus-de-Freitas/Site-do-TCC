efb42198542e0dd287fa7d1cae0fea98bb44262e
EA - Matheus De Freitas Alves <freitas_alves@educadventista.org>
2024-08-19 09:01:18 -0300

    atualização

--- src/components/Sobre nós/Superior.jsx
+++ src/components/Sobre nós/Superior.jsx
@@ -5,7 +5,7 @@ export default function Titulo() {
         <div className="flex justify-center items-center flex-col pb-10">
             <h1 className="lg:text-6xl font-inter-900 text-tittle-green font-black text-3xl pt-8 pb-16">Sobre nós</h1>
 
-            <img className="lg:w-128 w-48 rounded-xl" src={Nos} alt="Imagem da equipe" />
+            <img className="lg:w-128 w-72 rounded-xl" src={Nos} alt="Imagem da equipe" />
         </div>
 
         
